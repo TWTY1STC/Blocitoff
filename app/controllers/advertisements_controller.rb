4a9e8ba07027441b5b944c2df922d4866b488692
LizaN <liza.njuguna@gmail.com>
2016-03-14 18:44:41 +0000

    Ongoing Advert assignment

---
+++ app/controllers/advertisements_controller.rb
@@ -0,0 +1,28 @@
+class AdvertisementsController < ApplicationController
+  
+  def index
+    @advertisements = Advertisements.all
+  end
+
+  def show
+    @advertisement = Advertisement.find(params[:id])
+  end
+
+  def new
+    @advertisement = Advertisement.new
+  end
+
+  def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:post][:title]
+    @advertisement.body = params [:post][:body]
+    
+    if @advertisement.save
+      flash[:notice] = "Advertisement saved."
+      redirect_to @advertisement
+    else
+      flash.now[:alert] = "There was an error saving the advertisement. Please try again."
+      render :new
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -18,10 +18,24 @@ posts =  Post.all
     )
 end
 
+#create idempotent instance
 post = Post.find_or_create_by( title: "My unique post", body: "My unique body")
 comment = Comment.find_or_create_by(post: post, body: "My unique body")
+
+#create Advertisements
+50.times do
+    Advertisement.create!(
+        title: RandomData.random_sentence,
+        body: RandomData.random_paragraph
+    )
+end
+
+posts =  Post.all
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
+
     
