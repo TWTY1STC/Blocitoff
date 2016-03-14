8e2f18c0953f709bdd88a910cf8be19bee62a344
LizaN <liza.njuguna@gmail.com>
2016-03-14 21:57:37 +0000

    fix spam display

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,8 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
     
-     posts.each do |post|
-      if post_id % 5 == 0
+    @posts.each do |post|
+      if post.id % 5 == 0
         post.title = "SPAM"
       end
     end
