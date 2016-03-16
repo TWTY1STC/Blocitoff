09be6037a63bb1c92c5139dad14ad45cc70e6714
LizaN <liza.njuguna@gmail.com>
2016-03-16 21:00:11 +0000

    Nest posts within Topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 require 'random_data'
 
+#create Topics
+15.times do
+    Topic.create!(
+        name: RandomData.random_sentence,
+        description: RandomData.random_paragraph
+    )
+end
+topics = Topic.all
 #create Posts
 50.times do
     Post.create!(
+        topic: topics.sample,
         title: RandomData.random_sentence,
         body: RandomData.random_paragraph
     )
@@ -22,6 +31,7 @@ post = Post.find_or_create_by( title: "My unique post", body: "My unique body")
 comment = Comment.find_or_create_by(post: post, body: "My unique body")
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
     
