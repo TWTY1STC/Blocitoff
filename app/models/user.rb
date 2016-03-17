fcb731c42911c1225223dd125e319a8f88919f88
LizaN <liza.njuguna@gmail.com>
2016-03-17 13:32:53 +0000

    callback solution, needs test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase }
+      before_save {
+        name_array = name.split
+        name_array.each do |name|
+            name.capitalize!
+        end
+        name = name_array.join(" ")
+    }
     
     validates :name, length: {minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: {minimum: 6 }, if: "password_digest.nil?"
