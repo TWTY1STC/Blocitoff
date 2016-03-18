5f6011a1cc810e0df46893def0a4dda6c502af59
LizaN <liza.njuguna@gmail.com>
2016-03-18 12:04:36 +0000

    confirmation page for discussion

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,5 +20,22 @@ class UsersController < ApplicationController
       end
   end
   
+  def confirm
+    @user = User.new
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    @user.password = params[:user][:password]
+    @user.password_confirmation = params[:user][:password_confirmation]
+    
+    if @user.yes
+      redirect_to controller: "users", action: "create"
+    else
+      flash.now[:alert] = "There was an error creating your account. Please try again"
+      render :new
+
+    end
+      
+  end
+  
   
 end
