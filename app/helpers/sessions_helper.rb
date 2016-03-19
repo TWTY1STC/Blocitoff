9793b89429c73459af6519d7c1bac388922bcf1d
LizaN <liza.njuguna@gmail.com>
2016-03-19 17:28:35 +0000

    Success adding avatar image

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -14,4 +14,10 @@ module SessionsHelper
         User.find_by(id: session[:user_id])
     end
     #provides a shortcut in our code for anytime we need to find a user for the session
+
+    def avatar_url(user)
+        gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+        "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    end
+    
 end
