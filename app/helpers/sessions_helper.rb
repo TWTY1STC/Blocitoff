2219c2329bceac66884db60c5a60d31a1b07d62b
LizaN <liza.njuguna@gmail.com>
2016-03-19 15:46:13 +0000

    add sign in functionality

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,17 @@
+module SessionsHelper
+    
+    def create_session(user)
+        session[:user_id] = user.id
+    end
+    #sets user_id on the session object to user.id, can only have one user id per session
+    
+    def destroy_session(user)
+        session[:user_id] = nil
+    end
+    #clears the user id on the session object
+    
+    def current_user
+        User.find_by(id: session[:user_id])
+    end
+    #provides a shortcut in our code for anytime we need to find a user for the session
+end
