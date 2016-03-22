a08142882fc300a093cb369c8b535b4bb512f394
LizaN <liza.njuguna@gmail.com>
2016-03-22 13:07:09 +0000

    add functionalities for comments

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
+  
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
 
   get 'about' => 'welcome#about'
 
