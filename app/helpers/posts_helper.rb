5769ad83b544eb6aedee69a3cfe97a11f03cf572
LizaN <liza.njuguna@gmail.com>
2016-03-25 13:21:40 +0000

    moderator helper

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,5 +1,8 @@
 module PostsHelper
-    def user_is_authorized_for_post?(post)
+    def user_is_authorized_to_edit_post?(post)
+        current_user && (current_user == post.user || current_user.admin? || current.moderator?)
+    end
+    def user_is_authorized_to_delete_post?(post)
         current_user && (current_user == post.user || current_user.admin?)
     end
 end
