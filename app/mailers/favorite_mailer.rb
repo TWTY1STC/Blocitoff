2ff92b3a57313cbcb918f747fb32d530bf0df8f3
LizaN <liza.njuguna@gmail.com>
2016-03-29 22:28:36 +0000

    edit to email functionality

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -15,7 +15,7 @@ class FavoriteMailer < ApplicationMailer
     end
     
     def new_post(user, post)
-        headers["Message-ID"] = "<post/#{post.id}@secure-oasis-87797.herokuapp.com>"
+        headers["Message-ID"] = "<posts/#{post.id}@secure-oasis-87797.herokuapp.com>"
         headers["In-Reply_To"] = "<post/#{post.id}@secure-oasis-87797.herokuapp.com>"
         headers["References"] = "<post/#{post.id}>@secure-oasis-87797.herokuapp.com"
         
