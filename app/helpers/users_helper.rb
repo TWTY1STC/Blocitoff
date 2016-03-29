c5723c7651999c009219840ef742724d2330aa5a
LizaN <liza.njuguna@gmail.com>
2016-03-29 07:41:58 +0000

    refactor and add elements to user profile

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def user_activity_empty?
+        @user.comments.empty? && @user.posts.empty?
+    end
+    
+    def user_comments_empty?
+        @user.comments.empty?
+    end
+    
+    def user_posts_empty?
+        @user.comments.empty?
+    end
 end
