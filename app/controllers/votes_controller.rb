d480b97c0190b4695a6986d69b4e5e52859112a7
LizaN <liza.njuguna@gmail.com>
2016-03-30 13:23:39 +0000

    ongoing voter functionality

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,20 +3,18 @@ class VotesController < ApplicationController
   
   def up_vote
     update_vote(1)
-    redirect_to :back
     
     respond_to do |format|
-        format.html
+        format.html { redirect_to :back }
         format.js
     end
   end
   
   def down_vote
     update_vote(-1)
-    redirect_to :back
     
     respond_to do |format|
-        format.html
+        format.html { redirect_to :back }
         format.js
     end
   end
