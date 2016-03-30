3bb13683289d82bc0ec9c5690034f5568e774c36
LizaN <liza.njuguna@gmail.com>
2016-03-30 13:08:32 +0000

    work in progress

---
+++ spec/api/v1/controllers/comments_controller_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::CommentsController, type: :controller do, 
+
+    let(:my_topic) {create(:topic) }
+    let(:my_user) {create(:user) }
+    let(:my_post) {create(:post, topic: my_topic, user: my_user) }
+    let(:my_comment) { Comment.create!(body: 'Comment Body', post: my_post, user: my_user) }
+    
+    context "unauthenticated user" do
+        it "GET index returns http success" do
+            get :index
+            expect(response).to have_http_status(:success)
+        end
+        
+        it "GET show returns http success" do
+            get :show, post_id: my_post.id, comment.id: my_comment.id
+            expect(response).to have_http_status(:success)
+        end
+    end
+    
+    context "unauthorized user" do
+        it "GET index returns http success" do
+            get :index
+            expect(response).to have_http_status(:success)
+        end
+ 
+        it "GET show returns http success" do
+            get :show, post_id: my_post.id, comment.id: my_comment.id
+            expect(response).to have_http_status(:success)
+        end
+   end
+end
